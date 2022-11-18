962956b9abf65068a618c0f38df923d86b419f5a
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-18 15:27:22 +0530

    raindrops problem solved

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,12 @@
 def convert(number):
-    pass
+    s = ""
+    if number % 3 == 0:
+        s += "Pling" 
+    if number % 5 == 0:
+        s += "Plang" 
+    if number % 7 == 0:
+        s += "Plong" 
+
+    if s == '':
+        return str(number)
+    return s
