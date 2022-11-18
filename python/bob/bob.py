c87f4bdcb0195501f44084200ac7da594b994172
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-18 15:23:23 +0530

    bob problem solved

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,16 @@
-def response(hey_bob):
-    pass
+def response(hey_bob: str):
+    hey_bob = hey_bob.strip()
+    if hey_bob == "":
+        return "Fine. Be that way!"
+
+    if hey_bob.endswith("?"):
+        if hey_bob.isupper():
+            return "Calm down, I know what I'm doing!"
+
+        return "Sure."
+
+    if hey_bob.isupper():
+        return "Whoa, chill out!"
+
+    return "Whatever."
+
