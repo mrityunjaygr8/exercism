696839487c0dc9c300be029573bce78415dbd087
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-21 13:53:08 +0530

    darts problem solved

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,19 @@
+OUTER = 10
+MIDDLE = 5
+INNER = 1
+
+def in_inner(x, y):
+    if -INNER <= x <= INNER and -OUTER <= y <= OUTER:
+        return True
+
+    return False
+
+
 def score(x, y):
-    pass
+    if x ** 2 + y ** 2 <= INNER ** 2:
+        return 10
+    if x ** 2 + y ** 2 <= MIDDLE ** 2:
+        return 5
+    if x ** 2 + y ** 2 <= OUTER ** 2:
+        return 1
+    return 0
