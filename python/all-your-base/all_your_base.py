5f9bb19d3dcd5b0ce3bf3851125ff5643b2b2afa
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-21 15:58:53 +0530

    all-your-base problem solved

--- python/all-your-base/all_your_base.py
+++ python/all-your-base/all_your_base.py
@@ -1,2 +1,20 @@
 def rebase(input_base, digits, output_base):
-    pass
+    if input_base < 2:
+        raise ValueError("input base must be >= 2")
+    if output_base < 2:
+        raise ValueError("output base must be >= 2")
+    numbers = []
+    len_digits = len(digits)
+    for pow in range(len_digits):
+        if not (0 <= digits[pow] < input_base):
+            raise ValueError("all digits must satisfy 0 <= d < input base")
+        numbers.append((input_base ** (len_digits - pow - 1)) * digits[pow])
+    number = sum(numbers)
+    if number == 0:
+        return [0]
+    base = []
+    while number != 0:
+        base.append(number % output_base)
+        number = number // output_base
+
+    return base[::-1]
