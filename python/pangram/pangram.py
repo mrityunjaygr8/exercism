f3c19513a6b835c1c589e79cc2bf7cd2177cf402
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-22 12:46:57 +0530

    pangram problem solved

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
-def is_pangram(sentence):
-    pass
+from collections import Counter
+import string
+
+ALPHAS = [*string.ascii_lowercase]
+
+def is_pangram(sentence: str):
+    sentence = sentence.replace(".", "")
+    sentence = sentence.replace(" ", "")
+    sentence = sentence.lower()
+
+
+    counter = Counter(sentence)
+    return set(counter.keys()).issuperset(set(ALPHAS))
