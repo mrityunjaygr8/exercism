2eee5dfed3d349bd38c944b864096017b457fb5c
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-24 16:43:12 +0530

    resistor-color-duo problem solved

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,19 @@
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+
+}
 def value(colors):
-    pass
+    # bands = colors.split("-")
+    sum = COLORS[colors[0]] * 10
+    sum += COLORS[colors[1]]
+    return sum
+    # return int("".join(list([COLORS[bands[0]], COLORS[bands[1]]])))
