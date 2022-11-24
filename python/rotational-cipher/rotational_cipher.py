ac7ea9b4f6b37b9d8b53abbcb9f8fc677f756d1e
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-24 14:42:14 +0530

    rotational-cipher problem solved

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,21 @@
+from string import ascii_lowercase, ascii_uppercase
+
+LOWER = list(ascii_lowercase)
+UPPER = list(ascii_uppercase)
+
+
 def rotate(text, key):
-    pass
+    text = list(text)
+
+
+    for x in range(len(text)):
+        if text[x] not in UPPER and text[x] not in LOWER:
+            continue
+
+
+        if text[x].isupper():
+            text[x] = UPPER[(UPPER.index(text[x]) + key) % len(UPPER)]
+        else:
+            text[x] = LOWER[(LOWER.index(text[x]) + key) % len(LOWER)]
+
+    return "".join(text)
