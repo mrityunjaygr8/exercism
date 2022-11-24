a5a2131e258a6ef982fe0c6785130da9664c4ea9
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-24 15:56:40 +0530

    resistor-color problem solved

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,32 @@
+# COLORS = [
+#     "black",
+#     "brown",
+#     "red",
+#     "orange",
+#     "yellow",
+#     "green",
+#     "blue",
+#     "violet",
+#     "grey",
+#     "white",
+# ]
+
+
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
 def color_code(color):
-    pass
+    return COLORS[color]
 
 
 def colors():
-    pass
+    return list(COLORS.keys())
