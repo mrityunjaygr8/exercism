cecf3206b31a333d4955624533a035f842bb8d33
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-24 14:02:23 +0530

    isogram problem solved

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,10 @@
-def is_isogram(string):
-    pass
+from collections import Counter
+
+def is_isogram(string: str):
+    string = string.replace(" ", "")
+    string = string.replace("-", "")
+    string = string.lower()
+
+    c = Counter(string)
+
+    return len(c.keys()) == sum(c.values())
