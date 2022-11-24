e307a2475289b67746699e609b8c69346dc33088
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-24 16:53:32 +0530

    secret-handshake problem solved

--- python/secret-handshake/secret_handshake.py
+++ python/secret-handshake/secret_handshake.py
@@ -1,2 +1,19 @@
 def commands(binary_str):
-    pass
+    binary = list(binary_str)
+    output = list()
+
+    if binary[4] == "1":
+        output.append("wink")
+
+    if binary[3] == "1":
+        output.append("double blink")
+
+    if binary[2] == "1":
+        output.append("close your eyes")
+
+    if binary[1] == "1":
+        output.append("jump")
+
+    if binary[0] == "1":
+        output = list(reversed(output))
+    return output
